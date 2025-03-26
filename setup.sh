bb938d30cea3eb825d28b6da92d11cd30e859141
Dharshan <kdharshanprog@gmail.com>
2025-03-26 22:02:37 +0530

    Added some corrections.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
+mkdir models
 wget -O models/phi-2.Q4_K_M.gguf https://huggingface.co/TheBloke/phi-2-GGUF/resolve/main/phi-2.Q4_K_M.gguf 
 # Install requirements
-print_step "Installing requirements..."
 pip install -r requirements.txt
