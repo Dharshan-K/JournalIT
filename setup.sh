27213d42faeca5c1f0ee0ecc32000e2bd69a085c
Dharshan <kdharshanprog@gmail.com>
2025-03-26 21:45:21 +0530

    removed venv.

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ wget https://huggingface.co/TheBloke/phi-2-GGUF/resolve/main/phi-2.Q4_K_M.gguf
 # Install requirements
 print_step "Installing requirements..."
 pip install --upgrade pip
-pip install -r "$REQUIREMENTS_FILE"
+pip install -r requirements.txt
