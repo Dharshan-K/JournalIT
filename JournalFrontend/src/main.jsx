46793f440284cbfaf28b65865d53e987a819a8af
Dharshan <kdharshanprog@gmail.com>
2025-04-14 13:00:13 +0530

    Changed BrowserRouter to HashRouter.

--- JournalFrontend/src/main.jsx
+++ JournalFrontend/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-)
+  </HashRouter>
+);
